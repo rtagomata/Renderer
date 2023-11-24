54baf9bde828874f4664d91bb2fc1b58aca974fd
LegendsZ <akkshai@yahoo.com>
2023-11-23 21:25:35 -0500

    Revert "added stuff"
    This reverts commit b0614c5e80c9085d07e65b66b4da7e9330183551.

--- CallbackFunctions.cpp
+++ CallbackFunctions.cpp
@@ -1,9 +1,8 @@
 #include "CallbackFunctions.h"
-#include "RoomMesh.h"
-#include "VECTOR3D.h"
+
 namespace callbackFunctions {
 	int currentButton;
-	RoomMesh* roomMesh = NULL;
+
 	void display(void) {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glLoadIdentity();
